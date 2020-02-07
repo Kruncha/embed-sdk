4803ad4e38bc84c02fb3040262085454b7eba1b9
Will Scullin <scullin@scullin.com>
2020-02-07 14:14:29 -0800

    Initial work on embedding extensions.

--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -5,3 +5,7 @@ export const lookerHost = 'self-signed.looker.com:9999'
 export const dashboardId = 1
 // A Look that the user can see. Set to 0 to disable look.
 export const lookId = 1
+// An Explore that the user can see. Set to '' to disable explore.
+export const exploreId = 'thelook::products'
+// An Extension that the user can see. Set to '' to disable explore.
+export const extensionId = 'extension::my-great-extension'
